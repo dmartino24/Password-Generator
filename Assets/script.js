03db2d3e5129da6ac333a69919b19fabc3090d8f
Dustin Martin <d_man_24_3@hotmail.com>
2022-05-23 04:51:40 -0500

    added password function

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,20 @@
 // Assignment code here
-
-
+var getPasswordLength = function () {
+  var answer = prompt(
+    "Please set the preferred length of your password min 8 max 125 characters."
+  );
+  if (answer >= 8 && answer <= 125) {
+    return answer;
+  }
+  return getPasswordLength(answer);
+};
+var getCharacterTypes = function () {};
+var generatePassword = function () {
+  // length of password
+  var lengthPassword = getPasswordLength();
+  // character types
+  var characterTypes = getCharacterTypes();
+};
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
@@ -10,7 +24,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
